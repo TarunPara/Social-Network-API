480a5889baab8577179ce8bbeeb396a97703be98
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-13 00:59:25 -0400

    index

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Import the User model
+const User = require('./User');
+// Import the Thought model
+const Thought = require('./Thought');
+
+// Export the models for use elsewhere in the application
+module.exports = { User, Thought };
